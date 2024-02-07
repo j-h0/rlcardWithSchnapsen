74bdb3a41a1bf3ef95f03a7e6d9a5395df06cbc0
hu4361 <hu4361@mci4me.at>
2024-02-07 12:29:08 +0100

    add to dealer

--- rlcard/games/schnapsen/dealer.py
+++ rlcard/games/schnapsen/dealer.py
@@ -2,8 +2,8 @@ from .player import SchnapsenPlayer
 from .utils import utils as utils
 
 
-class GinRummyDealer:
-    ''' Initialize a GinRummy dealer class
+class SchnapsenDealer:
+    ''' Initialize a SchnapsenDealer class
     '''
     def __init__(self, np_random):
         ''' Empty discard_pile, set shuffled_deck, set stock_pile
@@ -13,14 +13,20 @@ class GinRummyDealer:
         self.shuffled_deck = utils.get_deck()  # keep a copy of the shuffled cards at start of new hand
         self.np_random.shuffle(self.shuffled_deck)
         self.stock_pile = self.shuffled_deck.copy()  # type: List[Card]
+        self.trump_card = None
 
-    def deal_cards(self, player: GinRummyPlayer, num: int):
+    def deal_cards(self, player: SchnapsenPlayer, num: int):
         ''' Deal some cards from stock_pile to one player
 
         Args:
-            player (GinRummyPlayer): The GinRummyPlayer object
+            player (SchnapsenPlayer): The SchnapsenPlayer object
             num (int): The number of cards to be dealt
         '''
         for _ in range(num):
             player.hand.append(self.stock_pile.pop())
         player.did_populate_hand()
+
+    def trumpExchange(SchnapsenCard):
+        old = self.trump_card
+        self.trump_card = SchnapsenCard
+        return old
